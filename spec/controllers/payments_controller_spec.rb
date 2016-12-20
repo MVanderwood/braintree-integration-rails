d2e6d2b66b98b05c2043e3b39f8d5b4b4a797172
MVanderwood <m.b.vanderwood@gmail.com>
2016-12-20 11:05:34 -0600

    Implement PaymentsController#new

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe PaymentsController, :type => :controller do
+  describe 'GET #new' do
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
 
+    it 'assigns a payment token to @client_token' do
+      expect(Braintree::ClientToken).to receive(:generate).and_return("your_client_token")
+      get :new
+      expect(assigns(:client_token)).to be_a(String)
+    end
+  end
 end

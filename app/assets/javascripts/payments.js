b341b55238b0c226bb3fe90e01adb8489cef30de
MVanderwood <m.b.vanderwood@gmail.com>
2016-12-22 02:14:13 -0600

    Refactor frontend Braintree integration

--- app/assets/javascripts/payments.js
+++ app/assets/javascripts/payments.js
@@ -1,2 +1,70 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+/* global braintree */
+(function() {
+
+    function BTPayment(token) {
+
+        var payment = this;
+        braintree.client.create({
+            authorization: token
+        }, function(clientError, clientInstance) {
+            if (clientError) return displayError('client');
+
+            payment.clientInstance = clientInstance;
+            createHostedFields();
+        });
+
+        function displayError(type) {
+            var errors = {
+                client: 'error in payment',
+                hostedField: 'error in hosted fields',
+                token: 'error with token'
+            };
+            document.getElementById('payment-error').innerHTML = errors[type];
+        }
+
+        function createHostedFields() {
+            braintree.hostedFields.create({
+                client: payment.clientInstance,
+                styles: {
+                    'input': { 'font-size': '14pt' },
+                    'input.invalid': { 'color': 'red' },
+                    'input.valid': { 'color': 'green' }
+                },
+                fields: {
+                    number: {
+                        selector: '#card-number',
+                        placeholder: '4111 1111 1111 1111'
+                    },
+                    cvv: {
+                        selector: '#cvv',
+                        placeholder: '123'
+                    },
+                    expirationDate: {
+                        selector: '#expiration-date',
+                        placeholder: '10/2019'
+                    }
+                }
+            }, function(hostedFieldError, hostedFieldInstance) {
+                window.error = hostedFieldError;
+                if (hostedFieldError) return displayError('hostedField');
+
+                document.getElementById('payment-submit').removeAttribute('disabled');
+
+                function submitForm(submitEvent) {
+                    submitEvent.preventDefault();
+                    hostedFieldInstance.tokenize(function(tokenError, payload) {
+                        if (tokenError) return displayError('token');
+
+                        document.getElementsByName('payment-method-nonce')[0].value = payload.nonce;
+                        payment.form.submit();
+                    });
+                }
+
+                payment.form = document.getElementById('payment-form');
+                payment.form.addEventListener('submit', submitForm, false);
+            });
+        }
+    }
+
+    window.BTPayment = BTPayment;
+})();
